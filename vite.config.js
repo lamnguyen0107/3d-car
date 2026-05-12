b2801443bf5c87c9dea08a1e7c949453776925db
lamnguyen0107 <lamnguyen051101@gmail.com>
2026-05-12 14:25:17 +0700

    ci(pages): add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 
+const isGitHubPagesBuild = process.env.GITHUB_PAGES === 'true';
+
 export default defineConfig({
+  base: isGitHubPagesBuild ? '/3d-car/' : '/',
   build: {
     rollupOptions: {
       output: {
